dc65e0aa3988b6e1a9a6a9b642a4f609446993d4
Raissa Miranda <raissa.miranda1801@gmail.com>
2026-03-13 21:59:47 -0300

    Add files via upload

---
+++ Aula05/Programa02/src/main.js
@@ -0,0 +1,31 @@
+// ------------------------------------------------------
+// -- Arquivo principal do jogo
+// ------------------------------------------------------
+
+// Importando o Phase
+import Phaser from 'phaser';
+
+const config = {
+    type: Phaser.AUTO,
+    width: 800,
+    height: 600,
+    scene: {
+        preload: preload,
+        create: create
+    }
+};
+
+// Instanciamento da cena
+const game = new Phaser.Game(config);
+
+// Preload de assets
+function preload() {
+    // Carrega a imagem do logo do Phaser
+    this.load.image('logo', 'assets/images/phaser-logo.png');
+}
+
+// Cria os objetos o jogo
+function create() {
+    // Adiciona a imagem na tela
+    this.add.image(400, 300, 'logo');
+}
